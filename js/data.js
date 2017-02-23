18a8c2a0ff88790a71f73e1095319ceb57806437
HQ.Wn <wanghongqing@gaiay.cn>
2017-02-23 11:16:13 +0800

    第一次提交方法fn1

---
+++ js/data.js
@@ -0,0 +1,14 @@
+/**
+ * Created by LJ on 2017/2/23.
+ */
+var vm = new Vue({
+  el:'#app',
+  data:{
+    'userId':''
+  },
+  methods:{
+    fn1:function () {
+      console.log("第一次修改");
+    }
+  }
+});
